ddb8642b294d374219cee2b43cb7003161212a5f
buffalosauce37 <league@Mac-mini.local>
2016-03-28 15:08:02 -0700

    I finished the Robot Program.

--- src/day1/robot/FourSquare.java
+++ src/day1/robot/FourSquare.java
@@ -5,27 +5,34 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class FourSquare {
 	
-	// 2. Create a new Robot
 	
-
+	Robot r2d2=new Robot();
 	void go() {
-		// 4. Make the robot move as fast as possible
-
+		
+r2d2.setSpeed(100);
 		// 5. Set the pen width to 5
-
+r2d2.setPenWidth(5);
 		// 6. Do steps #7 to #8 four times...
 
 			// 7. Set the pen color to random
-	
-			// 1. Call the drawSquare() method
+	for (int i = 0; i < 4; i++) {
+		r2d2.setRandomPenColor();
+		r2d2.turn(90);
+		drawSquare();
+	}
+		
 	
 			// 8. Turn the robot 90 degrees to the right
 
 	}
 
-	/* 3. Fill in the code to draw a square inside the method below. */
+
 	void drawSquare() {
-		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
+for (int i = 0; i < 4; i++) {
+	r2d2.penDown();
+	r2d2.move(300);
+	r2d2.turn(90);
+}
 
 		
 		
--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,9 +7,16 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+Robot r2d2=new Robot();
+r2d2.setSpeed(10000);
+r2d2.move(200);
+for (int i = 0; i < 8; i++) {
+r2d2.penDown();
+r2d2.setSpeed(10000);
+r2d2.setPenColor(Color.PINK);
+r2d2.turn(-360/8);
+r2d2.move(200);
+}
 
-	
-		
-		
-	}
+		}
 }
